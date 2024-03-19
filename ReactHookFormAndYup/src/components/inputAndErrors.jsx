6c2b47745f69e6441381dc66c93e2628fb5678f3
snkchan <144839872+snkchan@users.noreply.github.com>
2024-03-19 17:40:50 +0900

    feat : 공용컴포넌트 inputAndErrors 생성

---
+++ ReactHookFormAndYup/src/components/inputAndErrors.jsx
@@ -0,0 +1,21 @@
+const InputAndErrors = ({ register, resgisterKey, errors, ...rest }) => {
+  const INPUT_CSS =
+    "border-[0.3rem] border-solid border-theme-mainOppsite-light focus:bg-theme-mainOppsite-thick"
+  const SPAN_CSS =
+    "w-full h-[3rem] text-system-warning-thick text-ellipsis  overflow-hidden text-sm font-bold"
+  return (
+    <div className="w-[70%] flex flex-col ">
+      <input
+        className={INPUT_CSS}
+        autoComplete="off"
+        {...register(`${resgisterKey}`)}
+        {...rest}
+      />
+      <span className={SPAN_CSS}>
+        {errors[resgisterKey] && errors[resgisterKey].message}
+      </span>
+    </div>
+  )
+}
+
+export default InputAndErrors
