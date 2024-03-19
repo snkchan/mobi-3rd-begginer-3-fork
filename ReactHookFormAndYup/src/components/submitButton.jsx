4f49d3578f55c9caee205ba7c7a5bd8adde89df3
snkchan <144839872+snkchan@users.noreply.github.com>
2024-03-19 18:35:24 +0900

    feat : submitButton disable 기능 추가

--- ReactHookFormAndYup/src/components/submitButton.jsx
+++ ReactHookFormAndYup/src/components/submitButton.jsx
@@ -1,7 +1,13 @@
-const SubmitButton = ({ children, ...rest }) => {
+import { tailwindMerge } from "../utils"
+
+const SubmitButton = ({ children, disable, ...rest }) => {
   return (
     <button
-      className=" border-[0.3rem] border-solid text-center border-theme-mainOppsite-thick text-tiny font-bold rounded-full p-2 w-fit h-fit hover:bg-theme-mainOppsite-light"
+      disabled={!disable}
+      className={tailwindMerge(
+        " border-[0.3rem] border-solid text-center border-theme-mainOppsite-thick text-tiny font-bold rounded-full p-2 w-fit h-fit hover:bg-theme-mainOppsite-light",
+        !disable && "bg-grayScale-100 hover:bg-grayScale-100"
+      )}
       {...rest}
     >
       {children}
