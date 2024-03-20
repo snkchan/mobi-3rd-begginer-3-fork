436731a68d60743a653c34115da8b97794b46d45
snkchan <144839872+snkchan@users.noreply.github.com>
2024-03-20 22:53:53 +0900

    feat : boiler-template 공용컴포넌트 spacer생성

--- boiler-template/src/common/index.js
+++ boiler-template/src/common/index.js
@@ -2,3 +2,7 @@
  * 재사용
  * export * from "path" 형식으로 작성
  */
+
+import Spacer from './spacer'
+
+export { Spacer }
---
+++ boiler-template/src/common/spacer.jsx
@@ -0,0 +1,5 @@
+const Spacer = ({ w, h }) => {
+  return <div style={{ width: w, height: h, minHeight: '1rem', minWidth: '1rem' }}></div>
+}
+
+export default Spacer
